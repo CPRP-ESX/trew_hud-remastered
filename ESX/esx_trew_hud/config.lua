e6b15a61be9eaf7ae3e4474b4899f124fa8cd4fb
JordanBScott <99845881+JordanBScott@users.noreply.github.com>
2023-11-26 03:18:26 -0500

    Update config.lua

--- ESX/esx_trew_hud/config.lua
+++ ESX/esx_trew_hud/config.lua
@@ -2,7 +2,7 @@ Config = {}
 
 Config.Locale = 'en'
 
-Config.serverLogo = 'https://i.imgur.com/9a3Tj94.png'
+Config.serverLogo = 'https://cdn.discordapp.com/attachments/967905287673618512/1071688074444161084/CP__5_-removebg-preview_-_Copy.png?ex=6575634b&is=6562ee4b&hm=5e7e72b12d4dbc2a58a653c27c88ba450ba96f17cd10eabb39f21436be2dabc0&'
 
 Config.font = {
 	name 	= 'Signika Negative',
@@ -11,7 +11,7 @@ Config.font = {
 
 Config.date = {
 	format	 	= 'default',
-	AmPm		= false
+	AmPm		= true
 }
 
 Config.vehicle = {
@@ -19,7 +19,7 @@ Config.vehicle = {
 	maxSpeed = 180,
 
 	keys = {
-		seatbelt 	= 'K',
+		seatbelt 	= 'B',
 		cruiser		= 'CAPS',
 		signalLeft	= 'LEFT',
 		signalRight	= 'RIGHT',
@@ -28,13 +28,13 @@ Config.vehicle = {
 }
 
 Config.ui = {
-	showServerLogo		 = false,
+	showServerLogo		 = true,
 
 	showJob		 		 = true,
 
 	showWalletMoney 	 = true,
 	showBankMoney 		 = true,
-	showBlackMoney 		 = false,
+	showBlackMoney 		 = true,
 	showSocietyMoney	 = true,
 
 	showDate 			 = true,
